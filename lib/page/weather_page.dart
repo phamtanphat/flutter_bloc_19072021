49b3e4c602f77b230f73614926f8aa975d0e0176
phamtanphat <phatdroid94@gmail.com>
2021-08-20 19:24:23 +0700

    update code

--- lib/page/weather_page.dart
+++ lib/page/weather_page.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_19072021/repository/weather_repository.dart';
+import 'package:flutter_bloc_19072021/request/weather_request.dart';
 class WeatherPage extends StatefulWidget {
 
   @override
@@ -7,7 +9,19 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  late WeatherRepository repository;
 
+  @override
+  void initState() {
+    super.initState();
+    repository = WeatherRepository(weatherRequest: WeatherRequest());
+  }
+
+  @override
+  void didUpdateWidget(covariant WeatherPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    repository.fetchIdFromCityName("abc");
+  }
 
   @override
   Widget build(BuildContext context) {
